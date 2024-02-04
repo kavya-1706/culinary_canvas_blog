a18e609aaf25a386ab457ec9b755467b5eccc03b
Kavya Anil <kavya.anil@bds.christuniversity.in>
2024-02-04 17:50:04 +0530

    changed files

---
+++ script.js
@@ -0,0 +1,159 @@
+function recipe1(){
+
+   document.getElementById("recipe1").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("grid").style.display = "none";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("header").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+  
+}
+function back(){
+    document.getElementById("grid").style.display = "grid";
+    document.getElementById("header").style.display = "block"; 
+    document.getElementById("tips").style.display = "block"; 
+    document.getElementById("recipe").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("hideitem1").style.display = "none";
+    document.getElementById("hideitem2").style.display = "none";
+    document.getElementById("hideitem3").style.display = "none";
+    document.getElementById("allrecipe").style.display = "none";
+    document.getElementById("recipe1").style.display = "none";
+    document.getElementById("recipe2").style.display = "none";
+    document.getElementById("link2").style.display = "none";
+    document.getElementById("recipe6").style.display = "none";
+    document.getElementById("recipe7").style.display = "none";
+    document.getElementById("recipe8").style.display = "none";
+    document.getElementById("alllink").style.display = "none";
+    document.getElementById("recipe3").style.display = "none";
+    document.getElementById("recipe4").style.display = "none";
+    document.getElementById("recipe5").style.display = "none";  
+}
+function recipe2(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe2").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("header").style.display = "none";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+}
+function recipe6(){
+    document.getElementById("grid").style.display = "none";
+    document.getElementById("recipe6").style.display = "block";
+    document.getElementById("recipe").style.display = "block";
+    document.getElementById("header").style.display = "none";
+    document.getElementById("alllink").style.display = "none";
+    document.getElementById("allrecipe").style.display = "none";
+    document.getElementById("tips").style.display = "none";
+    document.getElementById("link2").style.display = "block";
+}
+function recipe3(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe3").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("header").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+}
+function recipe4(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe4").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("header").style.display = "none";
+   document.getElementById("alllink").style.display = "none";
+}
+function recipe5(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe5").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("header").style.display = "none";
+}
+function recipe7(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe7").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("header").style.display = "none";
+}
+
+function recipe8(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe8").style.display = "block";
+   document.getElementById("recipe").style.display = "block";
+   document.getElementById("alllink").style.display = "none";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("link2").style.display = "block";
+   document.getElementById("allrecipe").style.display = "none";
+   document.getElementById("header").style.display = "none";
+}
+function about(){
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe").style.display = "none";
+   document.getElementById("recipe1").style.display = "none";
+    document.getElementById("recipe2").style.display = "none";
+    document.getElementById("recipe3").style.display = "none";
+    document.getElementById("recipe4").style.display = "none";
+    document.getElementById("recipe5").style.display = "none";
+    document.getElementById("recipe6").style.display = "none";
+    document.getElementById("about").style.display = "block";
+    document.getElementById("header").style.display = "none";
+    document.getElementById("tips").style.display = "none";
+}
+function contact(){
+    document.getElementById("contact").style.display = "block";
+    document.getElementById("header").style.display = "none";
+    document.getElementById("tips").style.display = "none";
+    document.getElementById("grid").style.display = "none";
+   document.getElementById("recipe").style.display = "none";
+   document.getElementById("recipe1").style.display = "none";
+    document.getElementById("recipe2").style.display = "none";
+    document.getElementById("recipe3").style.display = "none";
+    document.getElementById("recipe4").style.display = "none";
+    document.getElementById("recipe5").style.display = "none";
+    document.getElementById("recipe6").style.display = "none";
+    document.getElementById("recipe7").style.display = "none";
+    document.getElementById("recipe8").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("link2").style.display = "none";
+
+   
+}
+function recipes(){
+    document.getElementById("grid").style.display = "grid";
+    document.getElementById("hideitem1").style.display = "grid";
+    document.getElementById("hideitem2").style.display = "grid";
+    document.getElementById("hideitem3").style.display = "grid";
+    document.getElementById("allrecipe").style.display = "block";
+    document.getElementById("alllink").style.display = "block";
+   document.getElementById("recipe").style.display = "none";
+   document.getElementById("tips").style.display = "none";
+   document.getElementById("recipe1").style.display = "none";
+   document.getElementById("header").style.display = "none";
+    document.getElementById("recipe2").style.display = "none";
+    document.getElementById("recipe3").style.display = "none";
+    document.getElementById("recipe4").style.display = "none";
+    document.getElementById("recipe5").style.display = "none";
+    document.getElementById("recipe6").style.display = "none";
+    document.getElementById("recipe7").style.display = "none";
+    document.getElementById("recipe8").style.display = "none";
+    document.getElementById("link2").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
